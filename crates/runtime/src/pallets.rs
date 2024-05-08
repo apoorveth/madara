96cb9e1d75d36ce8fb57f4533b618e05f16a490d
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2024-05-08 13:52:35 +0530

    kakarot backtest

--- crates/runtime/src/pallets.rs
+++ crates/runtime/src/pallets.rs
@@ -192,7 +192,7 @@ parameter_types! {
     pub const TransactionLongevity: u64 = u64::MAX;
     pub const ProtocolVersion: u8 = 0;
     pub const ProgramHash: Felt252Wrapper = SN_OS_PROGRAM_HASH;
-    pub const L1GasPrices: GasPrices = GasPrices { eth_l1_gas_price: unsafe { NonZeroU128::new_unchecked(10) }, strk_l1_gas_price: unsafe { NonZeroU128::new_unchecked(10) }, eth_l1_data_gas_price: unsafe { NonZeroU128::new_unchecked(10) }, strk_l1_data_gas_price: unsafe { NonZeroU128::new_unchecked(10) } };
+    pub const L1GasPrices: GasPrices = GasPrices { eth_l1_gas_price: unsafe { NonZeroU128::new_unchecked(52568862) }, strk_l1_gas_price: unsafe { NonZeroU128::new_unchecked(969724343) }, eth_l1_data_gas_price: unsafe { NonZeroU128::new_unchecked(52568862) }, strk_l1_data_gas_price: unsafe { NonZeroU128::new_unchecked(969724343) } };
     pub ExecutionConstants: Arc<VersionedConstants> = get_execution_constants();
 }
 
